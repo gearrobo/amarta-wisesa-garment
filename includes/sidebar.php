305982f149302c3d949789289306abdf28b165be
gatot nugroho <gearroboone@gmail.com>
2025-08-18 14:08:03 +0700

    ok

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,19 +1,19 @@
- <nav class="sidebar" id="sidebar">
+<nav class="sidebar" id="sidebar">
         <div class="sidebar-header">
             <h4>Amarta Wisesa</h4>
             <small>SPS Management System</small>
         </div>
         <ul class="sidebar-nav">
             <li>
-                <a href="index.php" class="nav-link active">
+                <a href="index.php" class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>">
                     <i class="fas fa-home"></i>
                     Dashboard
                 </a>
             </li>
             <li>
-                <a href="sps-sample.php" class="nav-link">
+                <a href="sps-sample.php" class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'sps-sample.php' ? 'active' : ''; ?>">
                     <i class="fas fa-file-alt"></i>
-                    Surat Perintah Sample
+                    Surat Perintah Samples
                 </a>
             </li>
             <li>
@@ -23,7 +23,7 @@
                 </a>
             </li>
             <li>
-                <a href="pengaturan.php" class="nav-link">
+                <a href="pengaturan.php" class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'pengaturan.php' ? 'active' : ''; ?>">
                     <i class="fas fa-cog"></i>
                     Pengaturan
                 </a>
