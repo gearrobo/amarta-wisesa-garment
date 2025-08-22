41fa9e0fee2b7c959c6ff077f9db8e28214e098c
gatot nugroho <gearroboone@gmail.com>
2025-08-22 20:07:55 +0700

    cv

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Surat Perintah Sample (SPS) - Amarta Wisesa</title>
+    <title>CV. Amarta Wisesa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css" rel="stylesheet">
