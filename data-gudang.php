fb438d4c35b91e44e7c8fe5f272edbde365d9962
gatot nugroho <gearroboone@gmail.com>
2025-08-22 18:53:53 +0700

    ok

--- data-gudang.php
+++ data-gudang.php
@@ -173,14 +173,14 @@ $gudangList = mysqli_fetch_all($result, MYSQLI_ASSOC);
                     <div class="card">
                         <div class="card-header d-flex justify-content-between align-items-center">
                             <h5 class="card-title mb-0">Daftar Gudang</h5>
-                            <div>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahGudangModal">
-                                    <i class="fas fa-plus"></i> <strong>TAMBAH DAFTAR GUDANG</strong>
-                                </button>
-                                <button type="button" class="btn btn-success">
-                                    <i class="fas fa-file-excel"></i> <strong>EXPORT EXCEL</strong>
-                                </button>
-                            </div>
+                        </div>
+                        <div>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahGudangModal">
+                                <i class="fas fa-plus"></i> <strong>TAMBAH DAFTAR GUDANG</strong>
+                            </button>
+                            <button type="button" class="btn btn-success">
+                                <i class="fas fa-file-excel"></i> <strong>EXPORT EXCEL</strong>
+                            </button>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
