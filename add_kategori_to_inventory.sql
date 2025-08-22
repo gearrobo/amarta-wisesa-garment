4eceb50c49078cb1f192f4ed0c6bf14f69901072
gatot nugroho <gearroboone@gmail.com>
2025-08-22 18:56:16 +0700

    ok

--- add_kategori_to_inventory.sql
+++
@@ -1,6 +0,0 @@
--- Add id_kategori column to inventory table
-ALTER TABLE inventory ADD COLUMN id_kategori INT NULL;
-
--- Add foreign key constraint
-ALTER TABLE inventory ADD CONSTRAINT fk_inventory_kategori 
-FOREIGN KEY (id_kategori) REFERENCES kategori_barang(id_kategori);
